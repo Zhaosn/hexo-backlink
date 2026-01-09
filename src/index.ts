a71f3fcece8c3557d35d6a197bc4b2226def8912
Zhaosn <zhaosn_dedsec@126.com>
2026-01-09 11:18:36 +0800

    fix: support handling files in subdirectories for link generation

--- src/index.ts
+++ src/index.ts
@@ -48,7 +48,12 @@ function action(data) {
       let anchor = null;
       [realName, anchor] = realName.split("#");
       let realNameExt = realName + ".md";
-      let file = fileList.find((file) => file.fileNameExt === realNameExt);
+      // Priority matching of complete paths, and files in folders are supported
+      let file = fileList.find((file) => file.articleName === realName);
+      // If not found, try matching the file name again (compatible with the original logic)
+      if (!file) {
+        file = fileList.find((file) => file.fileNameExt === realNameExt);
+      }
       if (file) {
         // If the target article was found. then replace the backlink with 'post_link'
         if (anchor) {
